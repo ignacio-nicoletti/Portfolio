73571a057bfb9b7092bbf453e79f72599a7f51bc
ignacio-nicoletti <109639879+ignacio-nicoletti@users.noreply.github.com>
2023-02-07 14:07:05 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Footer } from './components/Footer/Footer';
+import { Header } from './components/Header/Header';
+
+import { GlobalStyle } from './styles/global';
+import 'react-toastify/dist/ReactToastify.css';
+import { Main } from './components/Main/Main';
+function App() {
+  return (
+    <>
+          <GlobalStyle></GlobalStyle>
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
